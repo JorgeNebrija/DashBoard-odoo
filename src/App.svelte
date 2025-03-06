4ba6b56e259e8df40cb07597007b2386717e94af
Angel <angelgarcia05n@gmail.com>
2025-03-06 11:39:43 +0100

    cambio el app para que funcione el login

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,12 @@
 <script>
-import Router from "svelte-spa-router";
-  import { Route, navigate } from "svelte-routing";
+  import Router from "svelte-spa-router";
   import Login from "./lib/Login.svelte";
   import Inicio from "./lib/Inicio.svelte";
-  import { onMount } from "svelte";
 
-  onMount(() => {
-    // Si no hay pathname o está vacío, redirige a "/"
-    if (location.pathname === "") {
-      navigate("/", { replace: true });
-    }
-  });
+  const routes = {
+    "/": Login,
+    "/inicio": Inicio
+  };
 </script>
 
-<Router>
-  <Route path="/" component={Login} />
-  <Route path="/inicio" component={Inicio} />
-</Router>
+<Router {routes} />
