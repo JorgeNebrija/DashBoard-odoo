ebbe80715e5dc77489c28699e3e3fad1d5da2e86
Jorge <jorgeanton1810@gmail.com>
2025-03-07 12:20:22 +0100

    Fixed

--- src/lib/components/ProteccionRuta.svelte
+++ src/lib/components/ProteccionRuta.svelte
@@ -1,45 +1,32 @@
 <script>
-    import { usuario } from "../store";
-    import { push } from "svelte-spa-router";
-    import { onMount } from "svelte";
-  
-    export let componente; // Componente protegido
-  
-    let datosUsuario;
-    let cargando = true;
-  
-    onMount(() => {
-      usuario.subscribe((valor) => {
-        console.log("Valor del usuario en ProteccionRuta:", valor);
-        datosUsuario = valor;
-        cargando = false;
-  
-        if (!datosUsuario) {
-          console.log("Usuario no autenticado. Redirigiendo...");
-          push("/");
-        }
-      });
-    });
-  </script>
-  
-  {#if cargando}
-    <div class="cargando">
-      <p>Verificando acceso...</p>
-    </div>
-  {:else if datosUsuario}
-    <svelte:component this={componente} />
-  {:else}
-    <p>Redirigiendo...</p>
-  {/if}
-  
-  <style>
-    .cargando {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-      font-size: 1.2rem;
-      color: var(--color-primario);
+  import { usuario } from "../store";
+  import { push } from "svelte-spa-router";
+
+  export let componente; // Componente protegido
+
+  $: {
+    if ($usuario === null) {
+      console.log("Usuario no autenticado. Redirigiendo...");
+      push("/login"); // Redirigir a la página de login
     }
-  </style>
-  
+  }
+</script>
+
+{#if $usuario}
+  <svelte:component this={componente} />
+{:else}
+  <div class="cargando">
+    <p>Verificando acceso...</p>
+  </div>
+{/if}
+
+<style>
+  .cargando {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-size: 1.2rem;
+    color: var(--color-primario);
+  }
+</style>
